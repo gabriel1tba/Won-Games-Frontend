fcf508eb83e8eb7027e849f22c317d4cff52aa27
Gabriel Ferreira <gabriel.ferreira.itba@gmail.com>
2021-02-21 20:55:44 -0300

    feat: initial commit

--- src/components/Opa/index.tsx
+++
@@ -1,9 +0,0 @@
-import * as S from './styles'
-
-const Opa = () => (
-  <S.Wrapper>
-    <h1>Opa</h1>
-  </S.Wrapper>
-)
-
-export default Opa
--- src/components/Opa/stories.tsx
+++
@@ -1,9 +0,0 @@
-import { Story, Meta } from '@storybook/react/types-6-0'
-import Opa from '.'
-
-export default {
-  title: 'Opa',
-  component: Opa
-} as Meta
-
-export const Default: Story = () => <Opa />
