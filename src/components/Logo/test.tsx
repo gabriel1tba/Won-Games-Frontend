8f738f367fff054574eb8b136ec1c73abc8b5547
Gabriel Ferreira <gabriel.ferreira.itba@gmail.com>
2021-06-14 18:59:49 -0300

    feat: adicionando media queries

--- src/components/Logo/test.tsx
+++ src/components/Logo/test.tsx
@@ -1,5 +1,6 @@
 import { screen } from '@testing-library/react';
 import renderWithTheme from 'utils/tests/helpers';
+import 'jest-styled-components';
 
 import Logo from '.';
 
@@ -37,5 +38,15 @@ describe('<Logo />', () => {
     expect(screen.getByLabelText(/Won Games/i).parentElement).toHaveStyle({
       width: '11rem',
     });
+  });
+
+  it('should render a bigger logo without text on mobile', () => {
+    renderWithTheme(<Logo hideOnMobile />);
+
+    expect(screen.getByLabelText(/Won Games/i).parentElement).toHaveStyleRule(
+      'width',
+      '5.8rem',
+      { media: '(max-width: 768px)' },
+    );
   });
 });
