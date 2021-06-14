1f7aad327eca466c59d14c1074ea37db110d038d
Gabriel Ferreira <gabriel.ferreira.itba@gmail.com>
2021-06-14 17:10:13 -0300

    feat: criando test para cores diferentes

--- src/components/Logo/test.tsx
+++ src/components/Logo/test.tsx
@@ -14,4 +14,12 @@ describe('<Logo />', () => {
       color: '#FAFAFA',
     });
   });
+
+  it('should render a black label by default color', () => {
+    renderWithTheme(<Logo color="black" />);
+
+    expect(screen.getByLabelText(/Won Games/i).parentElement).toHaveStyle({
+      color: '#030517',
+    });
+  });
 });
