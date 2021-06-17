d146bc3c7bf73a38e94275d85e460b3f9b16f56a
Gabriel Ferreira <gabriel.ferreira.itba@gmail.com>
2021-06-17 20:11:47 -0300

    feat: button ajustando estilos quando link

--- src/components/Button/styles.ts
+++ src/components/Button/styles.ts
@@ -26,10 +26,6 @@ const wrapperModifiers = {
   `,
 
   withIcon: (theme: DefaultTheme) => css`
-    display: inline-flex;
-    align-items: center;
-    justify-content: center;
-
     svg {
       width: 1.5rem;
 
@@ -42,12 +38,16 @@ const wrapperModifiers = {
 
 export const Wrapper = styled.button<WrapperProps>`
   ${({ theme, size, fillWidth, hasIcon }) => css`
+    display: inline-flex;
+    align-items: center;
+    justify-content: center;
     cursor: pointer;
     background: linear-gradient(180deg, #ff5f5f 0%, #f062c0 50%);
     color: ${theme.colors.white};
     border: 0;
     border-radius: ${theme.border.radius};
     padding: ${theme.spacings.xxsmall};
+    text-decoration: none;
 
     &:hover {
       background: linear-gradient(180deg, #e35565 0%, #d958a6 50%);
