8b3133e632e8ad60d6953d3bfea9f125c956219b
Gabriel <gabriel.ferreira.itba@gmail.com>
2021-07-13 17:05:49 -0300

    feat: componente CardGame criando função onFav

--- src/components/GameCard/index.tsx
+++ src/components/GameCard/index.tsx
@@ -1,5 +1,6 @@
 import {
   AddShoppingCart,
+  Favorite,
   FavoriteBorder,
 } from '@styled-icons/material-outlined';
 import Button from 'components/Button';
@@ -11,6 +12,8 @@ export type GameCardProps = {
   img: string;
   price: string;
   promotionalPrice?: string;
+  favorite?: boolean;
+  onFav?: () => void;
 };
 
 const GameCard = ({
@@ -19,6 +22,8 @@ const GameCard = ({
   img,
   price,
   promotionalPrice,
+  favorite = false,
+  onFav,
 }: GameCardProps) => (
   <S.Wrapper>
     <S.ImageBox>
@@ -30,8 +35,12 @@ const GameCard = ({
         <S.Title>{title}</S.Title>
         <S.Developer>{developer}</S.Developer>
       </S.Info>
-      <S.FavButton role="button">
-        <FavoriteBorder aria-label="Add to Wishlist" />
+      <S.FavButton onClick={onFav} role="button">
+        {favorite ? (
+          <Favorite aria-label="Remove from Wishlist" />
+        ) : (
+          <FavoriteBorder aria-label="Add to Wishlist" />
+        )}
       </S.FavButton>
       <S.BuyBox>
         {!!promotionalPrice && <S.Price isPromotional>{price}</S.Price>}
