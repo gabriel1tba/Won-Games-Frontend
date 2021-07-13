43fb73f16c80ba5c65c78f4a2f2edafd382296e3
Gabriel <gabriel.ferreira.itba@gmail.com>
2021-07-13 15:55:09 -0300

    feat: componente CardGame test para preço promocional

--- src/components/GameCard/styles.ts
+++ src/components/GameCard/styles.ts
@@ -24,25 +24,74 @@ export const ImageBox = styled.div`
   );
   background-size: 80rem 14rem;
   animation: placeholderShimmer 1s linear infinite forwards;
-`;
-
-export const Content = styled.div``;
-
-export const Info = styled.div`
   img {
     width: 100%;
     height: 100%;
     object-fit: cover;
   }
+  @keyframes placeholderShimmer {
+    0% {
+      background-position: -40rem 0;
+    }
+    100% {
+      background-position: 40rem 0;
+    }
+  }
 `;
 
-export const Title = styled.h3``;
+export const Content = styled.div`
+  ${({ theme }) => css`
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+    position: relative;
+    height: 100%;
+    margin: ${theme.spacings.xsmall};
+  `}
+`;
 
-export const Developer = styled.h4``;
+export const Info = styled.div`
+  max-width: calc(100% - 2.5rem);
+`;
 
-export const FavButton = styled.div``;
+export const Title = styled.h3`
+  ${({ theme }) => css`
+    font-size: ${theme.font.sizes.medium};
+    line-height: ${theme.font.sizes.medium};
+    font-weight: ${theme.font.bold};
+    color: ${theme.colors.black};
+  `}
+`;
 
-export const BuyBox = styled.div``;
+export const Developer = styled.h4`
+  ${({ theme }) => css`
+    font-size: ${theme.font.sizes.small};
+    font-weight: ${theme.font.bold};
+    color: ${theme.colors.gray};
+  `}
+`;
+
+export const FavButton = styled.div`
+  ${({ theme }) => css`
+    color: ${theme.colors.primary};
+    position: absolute;
+    right: 0;
+    top: -0.5rem;
+    cursor: pointer;
+    svg {
+      width: 2.5rem;
+    }
+  `}
+`;
+
+export const BuyBox = styled.div`
+  ${({ theme }) => css`
+    display: flex;
+    align-items: center;
+    justify-content: flex-end;
+    margin-top: ${theme.spacings.xxsmall};
+  `}
+`;
 
 type PriceProps = {
   isPromotional?: boolean;
