666a6e441afe8344ae924a0896e4684f0b4df744
Gabriel <gabriel.ferreira.itba@gmail.com>
2021-07-13 10:17:20 -0300

    feat: componente Highlight tests e stories

--- src/components/Highlight/stories.tsx
+++ src/components/Highlight/stories.tsx
@@ -1,9 +1,15 @@
 import { Story, Meta } from '@storybook/react/types-6-0';
-import Highlight from '.';
+import Highlight, { HighlightProps } from '.';
 
 export default {
   title: 'Highlight',
   component: Highlight,
+  args: {
+    title: 'Read Dead its back',
+    subtitle: 'Come see John new adventures',
+    buttonLabel: 'Buy now',
+    buttonLink: '/rdr2',
+  },
 } as Meta;
 
-export const Default: Story = () => <Highlight />;
+export const Default: Story<HighlightProps> = (args) => <Highlight {...args} />;
--- src/components/Highlight/test.tsx
+++ src/components/Highlight/test.tsx
@@ -3,12 +3,25 @@ import renderWithTheme from 'utils/tests/helpers';
 
 import Highlight from '.';
 
+const props = {
+  title: 'Heading 1',
+  subtitle: 'Heading 2',
+  buttonLabel: 'Buy now',
+  buttonLink: '/rdr2',
+};
+
 describe('<Highlight />', () => {
   it('should render the heading', () => {
-    renderWithTheme(<Highlight />);
+    renderWithTheme(<Highlight {...props} />);
 
     expect(
-      screen.getByRole('heading', { name: /Highlight/i }),
+      screen.getByRole('heading', { name: /Heading 1/i }),
     ).toBeInTheDocument();
+
+    expect(
+      screen.getByRole('heading', { name: /Heading 2/i }),
+    ).toBeInTheDocument();
+
+    expect(screen.getByRole('link', { name: /Buy now/i })).toBeInTheDocument();
   });
 });
