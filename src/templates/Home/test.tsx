f668bed395b69d69de37c67b195989f98f4ac6fe
Gabriel <gabriel.ferreira.itba@gmail.com>
2021-07-17 15:43:41 -0300

    feat: page Home adicionando Menu e footer

--- src/templates/Home/test.tsx
+++ src/templates/Home/test.tsx
@@ -1,13 +1,33 @@
-import { render, screen } from '@testing-library/react';
+import { screen } from '@testing-library/react';
+import renderWithTheme from 'utils/tests/helpers';
 
 import Home from '.';
 
 describe('<Home />', () => {
-  it('should render the heading', () => {
-    const { container } = render(<Home />);
+  it('should render menu and footer', () => {
+    renderWithTheme(<Home />);
 
-    expect(screen.getByRole('heading', { name: /Home/i })).toBeInTheDocument();
+    // Testando Menu
+    expect(screen.getByLabelText(/open menu/i)).toBeInTheDocument();
 
-    expect(container.firstChild).toMatchSnapshot();
+    // Testando Footer
+    expect(
+      screen.getByRole('heading', { name: /contact us/i }),
+    ).toBeInTheDocument();
+  });
+
+  it('should render the sections', () => {
+    renderWithTheme(<Home />);
+
+    expect(screen.getByRole('heading', { name: /News/i })).toBeInTheDocument();
+    expect(
+      screen.getByRole('heading', { name: /Most Popular/i }),
+    ).toBeInTheDocument();
+    expect(
+      screen.getByRole('heading', { name: /Upcomming/i }),
+    ).toBeInTheDocument();
+    expect(
+      screen.getByRole('heading', { name: /Free Games/i }),
+    ).toBeInTheDocument();
   });
 });
