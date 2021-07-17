495c51593c9662bbe1651bc8d7afea198b5fffdd
Gabriel <gabriel.ferreira.itba@gmail.com>
2021-07-17 08:56:14 -0300

    feat: component GameCardSlider conf dos sliders

--- src/components/GameCardSlider/index.tsx
+++ src/components/GameCardSlider/index.tsx
@@ -14,8 +14,41 @@ type GameCardSliderProps = {
 };
 
 const settings: SliderSettings = {
-  nextArrow: <ArrowRight aria-label="next game" />,
-  prevArrow: <ArrowLeft aria-label="previus game" />,
+  slidesToShow: 4,
+  infinite: false,
+  lazyLoad: 'ondemand',
+  responsive: [
+    {
+      breakpoint: 1375,
+      settings: {
+        arrows: false,
+        slidesToShow: 3.2,
+      },
+    },
+    {
+      breakpoint: 1024,
+      settings: {
+        arrows: false,
+        slidesToShow: 2.2,
+      },
+    },
+    {
+      breakpoint: 570,
+      settings: {
+        arrows: false,
+        slidesToShow: 1.2,
+      },
+    },
+    {
+      breakpoint: 375,
+      settings: {
+        arrows: false,
+        slidesToShow: 1.1,
+      },
+    },
+  ],
+  nextArrow: <ArrowRight aria-label="next games" />,
+  prevArrow: <ArrowLeft aria-label="previous games" />,
 };
 
 const GameCardSlider = ({ items, color = 'black' }: GameCardSliderProps) => (
