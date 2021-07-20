a25fc93495aa636312e4ee338d82a609dc585cfb
Gabriel <gabriel.ferreira.itba@gmail.com>
2021-07-20 08:03:13 -0300

    feat: component TextField test style e lógica

--- src/components/Textfield/index.tsx
+++ src/components/Textfield/index.tsx
@@ -1,9 +1,38 @@
+import { useState, InputHTMLAttributes } from 'react';
+
 import * as S from './styles';
 
-const Textfield = () => (
-  <S.Wrapper>
-    <h1>Textfield</h1>
-  </S.Wrapper>
-);
+export type TextFieldProps = {
+  onInput?: (value: string) => void;
+  label?: string;
+  labelFor?: string;
+  initialValue?: string;
+} & InputHTMLAttributes<HTMLInputElement>;
 
-export default Textfield;
+const TextField = ({
+  label,
+  labelFor = '',
+  initialValue = '',
+  onInput,
+  ...props
+}: TextFieldProps) => {
+  const [value, setValue] = useState(initialValue);
+
+  const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const newValue = e.currentTarget.value;
+    setValue(newValue);
+
+    !!onInput && onInput(newValue);
+  };
+
+  return (
+    <S.Wrapper>
+      {!!label && <S.Label htmlFor={labelFor}>{label}</S.Label>}
+      <S.InputWrapper>
+        <S.Input type="text" onChange={onChange} value={value} {...props} />
+      </S.InputWrapper>
+    </S.Wrapper>
+  );
+};
+
+export default TextField;
--- src/components/Textfield/stories.tsx
+++ src/components/Textfield/stories.tsx
@@ -1,9 +1,23 @@
 import { Story, Meta } from '@storybook/react/types-6-0';
-import Textfield from '.';
+import TextField, { TextFieldProps } from '.';
 
 export default {
-  title: 'Textfield',
-  component: Textfield,
+  title: 'TextField',
+  component: TextField,
+  args: {
+    label: 'E-mail',
+    labelFor: 'Email',
+    id: 'Email',
+    initialValue: '',
+    placeholder: 'john.cage@gmail.com',
+  },
+  argTypes: {
+    onInput: { action: 'changed' },
+  },
 } as Meta;
 
-export const Default: Story = () => <Textfield />;
+export const Default: Story<TextFieldProps> = (args) => (
+  <div style={{ maxWidth: 300, padding: 15 }}>
+    <TextField {...args} />
+  </div>
+);
