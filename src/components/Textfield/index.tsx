d900b22d99b938e79588db7b400773ebe7c0d07a
Gabriel <gabriel.ferreira.itba@gmail.com>
2021-07-20 08:12:37 -0300

    feat: component TextField test style e lógica

--- src/components/Textfield/index.tsx
+++ src/components/Textfield/index.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState, InputHTMLAttributes } from 'react';
 
 import * as S from './styles';
 
@@ -7,7 +7,7 @@ export type TextFieldProps = {
   label?: string;
   labelFor?: string;
   initialValue?: string;
-} & React.InputHTMLAttributes<HTMLInputElement>;
+} & InputHTMLAttributes<HTMLInputElement>;
 
 const TextField = ({
   label,
