aaa87ccd1f805eb94602a08aad7ae6ad199158dd
Gabriel <gabriel.ferreira.itba@gmail.com>
2021-07-28 16:30:04 -0300

    feat: componente FormSignUp tests

---
+++ src/components/FormSignIn/test.tsx
@@ -0,0 +1,40 @@
+import { screen } from '@testing-library/react';
+import renderWithTheme from 'utils/tests/helpers';
+
+import FormSignIn from '.';
+
+describe('<FormSignIn />', () => {
+  it('should render the heading', () => {
+    renderWithTheme(<FormSignIn />);
+
+    // verifique email
+    expect(screen.getByPlaceholderText(/email/i)).toBeInTheDocument();
+
+    // verifique password
+    expect(screen.getByPlaceholderText(/password/i)).toBeInTheDocument();
+
+    // verifique button
+    expect(
+      screen.getByRole('button', { name: /Sign in now/i }),
+    ).toBeInTheDocument();
+  });
+
+  it('should render the forgot password link', () => {
+    renderWithTheme(<FormSignIn />);
+
+    // link
+    expect(
+      screen.getByRole('link', { name: /Forgot your password?/i }),
+    ).toBeInTheDocument();
+  });
+
+  it('should render the text and link to sign up', () => {
+    renderWithTheme(<FormSignIn />);
+
+    // text
+    expect(screen.getByText(/Don’t have an account?/i)).toBeInTheDocument();
+
+    // link
+    expect(screen.getByRole('link', { name: /Sign up/i })).toBeInTheDocument();
+  });
+});
