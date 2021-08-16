a5f95a63dc7306acbe5fcaa113eec575fc07416d
Gabriel Ferreira <gabriel.ferreira.itba@gmail.com>
2021-08-16 19:22:28 -0300

    feat: componente GameDetails finish

--- src/components/GameDetails/stories.tsx
+++ src/components/GameDetails/stories.tsx
@@ -1,6 +1,7 @@
 import { Story, Meta } from '@storybook/react/types-6-0';
-
 import GameDetails, { GameDetailsProps } from '.';
+
+import mock from './mock';
 
 export default {
   title: 'Game/GameDetails',
@@ -10,14 +11,21 @@ export default {
       default: 'won-dark',
     },
   },
-  args: {
-    platforms: ['windows', 'linux', 'mac'],
-  },
+  args: mock,
   argTypes: {
+    releaseDate: {
+      control: 'date',
+    },
     platforms: {
       control: {
         type: 'inline-check',
         options: ['windows', 'linux', 'mac'],
+      },
+    },
+    genres: {
+      control: {
+        type: 'inline-check',
+        options: ['Role-playing', 'Narrative'],
       },
     },
   },
