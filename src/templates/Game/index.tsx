f6da22b7ea36852b071addeb43885930aa3610f4
Gabriel Ferreira <gabriel.ferreira.itba@gmail.com>
2021-09-25 15:48:05 -0300

    feat: adicionando GameInfo ao template Game

--- src/templates/Game/index.tsx
+++ src/templates/Game/index.tsx
@@ -1,10 +1,22 @@
+import GameInfo, { GameInfoProps } from 'components/GameInfo';
 import Base from 'templates/Base';
 
 import * as S from './styles';
 
-const Game = () => (
+export type GameTemplateProps = {
+  cover: string;
+  gameInfo: GameInfoProps;
+};
+
+const Game = ({ cover, gameInfo }: GameTemplateProps) => (
   <Base>
-    <h1>Game</h1>
+    <S.Cover src={cover} role="image" aria-label="cover" />
+
+    <S.Main>
+      <S.SectionGameInfo>
+        <GameInfo {...gameInfo} />
+      </S.SectionGameInfo>
+    </S.Main>
   </Base>
 );
 
--- src/templates/Game/styles.ts
+++ src/templates/Game/styles.ts
@@ -1,3 +1,44 @@
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
+import media from 'styled-media-query';
 
-export const Wrapper = styled.main``;
+import Container from 'components/Container';
+
+type CoverProps = {
+  src: string;
+};
+
+export const Main = styled.main`
+  margin-top: 20rem;
+  ${media.greaterThan('medium')`
+    margin-top: 58rem;
+  `}
+`;
+
+export const Cover = styled.div<CoverProps>`
+  ${({ src }) => css`
+    position: absolute;
+    top: 0;
+    right: 0;
+    left: 0;
+    height: 39.5rem;
+    background-image: url(${src});
+    background-size: cover;
+    background-position: top center;
+    opacity: 0.4;
+    ${media.greaterThan('medium')`
+      height: 70rem;
+      clip-path: polygon(0 0, 100% 0, 100% 100%, 0 85%);
+    `}
+  `}
+`;
+
+const Section = styled(Container).attrs({ as: 'section' })`
+  ${({ theme }) => css`
+    margin-bottom: ${theme.spacings.xlarge};
+    ${media.greaterThan('medium')`
+      margin-bottom: calc(${theme.spacings.xlarge} * 2);
+    `}
+  `}
+`;
+
+export const SectionGameInfo = styled(Section)``;
