43c5593717a1b5693f855cb922147372bc214f01
Gabriel Ferreira <gabriel.ferreira.itba@gmail.com>
2021-09-25 16:36:05 -0300

    feat: adicionando TextContent ao Game template

--- src/templates/Game/index.tsx
+++ src/templates/Game/index.tsx
@@ -2,15 +2,18 @@ import GameInfo, { GameInfoProps } from 'components/GameInfo';
 import Gallery, { GalleryImageProps } from 'components/Gallery';
 import Base from 'templates/Base';
 
+import TextContent from 'components/TextContent';
+
 import * as S from './styles';
 
 export type GameTemplateProps = {
   cover: string;
   gameInfo: GameInfoProps;
   gallery?: GalleryImageProps[];
+  description: string;
 };
 
-const Game = ({ cover, gameInfo, gallery }: GameTemplateProps) => (
+const Game = ({ cover, gameInfo, gallery, description }: GameTemplateProps) => (
   <Base>
     <S.Cover src={cover} role="image" aria-label="cover" />
 
@@ -18,9 +21,14 @@ const Game = ({ cover, gameInfo, gallery }: GameTemplateProps) => (
       <S.SectionGameInfo>
         <GameInfo {...gameInfo} />
       </S.SectionGameInfo>
+
       <S.SectionGallery>
         {!!gallery && <Gallery items={gallery} />}
       </S.SectionGallery>
+
+      <S.SectionDescription>
+        <TextContent title="Description" content={description} />
+      </S.SectionDescription>
     </S.Main>
   </Base>
 );
