82686976cb6dadf609ba6ba67888372c1fc79e34
Gabriel Ferreira <gabriel.ferreira.itba@gmail.com>
2021-09-25 16:39:43 -0300

    feat: adicionando GameDetails ao Game template

--- src/pages/game/[slug].tsx
+++ src/pages/game/[slug].tsx
@@ -44,6 +44,14 @@ export async function getStaticProps() {
       },
       gallery: galleryMock,
       description: descriptionHTML,
+      details: {
+        developer: 'CD PROJEKT RED',
+        releaseDate: '2020-12-10T23:00:00',
+        platforms: ['windows'],
+        publisher: 'CD PROJEKT RED',
+        rating: 'BR18',
+        genres: ['Action', 'Role-playing'],
+      },
     },
   };
 }
