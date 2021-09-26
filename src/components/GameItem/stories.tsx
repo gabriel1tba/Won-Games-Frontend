494b94c1bee18816f57804e1dd71e8972bbb973a
Gabriel Ferreira <gabriel.ferreira.itba@gmail.com>
2021-09-26 13:56:00 -0300

    feat: GameItem exibindo informações de pagamento

--- src/components/GameItem/stories.tsx
+++ src/components/GameItem/stories.tsx
@@ -19,4 +19,10 @@ export const WithPayment: Story<GameItemProps> = (args) => (
 
 WithPayment.args = {
   downloadLink: 'https://wongames.com/game/download/21312ndasd',
+  paymentInfo: {
+    flag: 'mastercard',
+    img: '/img/master-card.png',
+    number: '**** **** **** 4326',
+    purchaseDate: 'Purchase made on 07/20/2020 at 20:32',
+  },
 };
