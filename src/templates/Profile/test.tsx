a6ee06b3ad9dcf932862bb56d2294d364a4586ca
Gabriel Ferreira <gabriel.ferreira.itba@gmail.com>
2021-09-29 21:44:50 -0300

    feat: activeLink with useRouter

--- src/templates/Profile/test.tsx
+++ src/templates/Profile/test.tsx
@@ -1,18 +1,40 @@
+import 'match-media-mock';
 import { screen } from '@testing-library/react';
 import renderWithTheme from 'utils/tests/helpers';
 
 import Profile from '.';
 
-describe('<Profile />', () => {
-  it('should render the heading', () => {
-    renderWithTheme(
-      <Profile>
-        <span />
-      </Profile>,
-    );
+jest.mock('next/router', () => ({
+  useRouter: jest.fn(() => ({ asPath: '/profile/me' })),
+}));
 
-    expect(
-      screen.getByRole('heading', { name: /my profile/i }),
-    ).toBeInTheDocument();
+jest.mock('templates/Base', () => ({
+  __esModule: true,
+  default: function Mock({ children }: { children: React.ReactNode }) {
+    return <div data-testid="Mock Base">{children}</div>;
+  },
+}));
+
+jest.mock('components/Heading', () => ({
+  __esModule: true,
+  default: function Mock({ children }: { children: React.ReactNode }) {
+    return <div data-testid="Mock Heading">{children}</div>;
+  },
+}));
+
+jest.mock('components/ProfileMenu', () => ({
+  __esModule: true,
+  default: function Mock() {
+    return <div data-testid="Mock ProfileMenu" />;
+  },
+}));
+
+describe('<Profile />', () => {
+  it('should render sections', () => {
+    renderWithTheme(<Profile>Lorem Ipsum</Profile>);
+
+    expect(screen.getByText('Lorem Ipsum')).toBeInTheDocument();
+    expect(screen.getByText('My profile')).toBeInTheDocument();
+    expect(screen.getByTestId('Mock ProfileMenu')).toBeInTheDocument();
   });
 });
