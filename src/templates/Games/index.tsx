678787b3f0a06b2be59889a438b4ffd2fc3bd41b
Gabriel Ferreira <gabriel.ferreira.itba@gmail.com>
2021-10-09 15:53:27 -0300

    feat: criando custom hook useQueryGames

--- src/templates/Games/index.tsx
+++ src/templates/Games/index.tsx
@@ -1,4 +1,3 @@
-import { useQuery } from '@apollo/client';
 import Base from 'templates/Base';
 import { KeyboardArrowDown as ArrowDown } from '@styled-icons/material-outlined/KeyboardArrowDown';
 
@@ -8,8 +7,8 @@ import Grid from 'components/Grid';
 
 import * as S from './styles';
 
-import { QueryGames, QueryGamesVariables } from 'graphql/generated/QueryGames';
-import { QUERY_GAMES } from 'graphql/queries/games';
+import { useQueryGames } from 'graphql/queries/games';
+
 import Loader from 'components/Loader';
 
 export type GamesTemplateProps = {
@@ -18,10 +17,9 @@ export type GamesTemplateProps = {
 };
 
 const GamesTemplate = ({ filterItems }: GamesTemplateProps) => {
-  const { data, loading, fetchMore } = useQuery<
-    QueryGames,
-    QueryGamesVariables
-  >(QUERY_GAMES, { variables: { limit: 15 } });
+  const { data, loading, fetchMore } = useQueryGames({
+    variables: { limit: 15 },
+  });
 
   const handleFilter = () => {
     return;
