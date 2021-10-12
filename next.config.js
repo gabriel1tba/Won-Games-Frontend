01b98e8dc36166dab8095cd5d397ff61af4d4613
Gabriel Ferreira <gabriel.ferreira.itba@gmail.com>
2021-10-12 15:12:45 -0300

    fix: next build

--- next.config.js
+++ next.config.js
@@ -1,13 +1,13 @@
 // eslint-disable-next-line @typescript-eslint/no-var-requires
-const withPWA = require('next-pwa')
-const isProd = process.env.NODE_ENV === 'production'
+const withPWA = require('next-pwa');
+const isProd = process.env.NODE_ENV === 'production';
 
 module.exports = withPWA({
   pwa: {
     dest: 'public',
-    disable: !isProd
+    disable: !isProd,
   },
-  future: {
-    webpack5: true
-  }
-})
+  images: {
+    domains: ['localhost', 'res.cloudinary.com'],
+  },
+});
