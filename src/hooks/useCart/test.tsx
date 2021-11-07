5f8dbc80b8af8390d6a9846f9a00f88c384e7738
Gabriel Ferreira <gabriel.ferreira.itba@gmail.com>
2021-11-07 08:33:27 -0300

    feat: criando métodos de total e quantity

--- src/hooks/useCart/test.tsx
+++ src/hooks/useCart/test.tsx
@@ -10,6 +10,20 @@ import useCart from '.';
 import { cartItems, gamesMock } from './mock';
 
 describe('useCart', () => {
+  it('should display an error when not passing the provider', () => {
+    const wrapper = ({ children }: CartProviderProps) => (
+      <MockedProvider mocks={[gamesMock]}>{children}</MockedProvider>
+    );
+
+    setStorageItem('cartItems', ['1', '2']);
+
+    const { result } = renderHook(() => useCart(), {
+      wrapper,
+    });
+
+    expect(result.error).toStrictEqual(undefined);
+  });
+
   it('should return items and its info if there are any in the cart', async () => {
     const wrapper = ({ children }: CartProviderProps) => (
       <MockedProvider mocks={[gamesMock]}>
@@ -26,19 +40,7 @@ describe('useCart', () => {
     await waitForNextUpdate();
 
     expect(result.current.items).toStrictEqual(cartItems);
-  });
-
-  it('should display an error when not passing the provider', () => {
-    const wrapper = ({ children }: CartProviderProps) => (
-      <MockedProvider mocks={[gamesMock]}>{children}</MockedProvider>
-    );
-
-    setStorageItem('cartItems', ['1', '2']);
-
-    const { result } = renderHook(() => useCart(), {
-      wrapper,
-    });
-
-    expect(result.error).toStrictEqual(undefined);
+    expect(result.current.quantity).toBe(2);
+    expect(result.current.total).toBe('$21.00');
   });
 });
