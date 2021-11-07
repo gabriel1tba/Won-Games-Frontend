299a33491128334a4a4686ad9dcae685efc74c32
Gabriel Ferreira <gabriel.ferreira.itba@gmail.com>
2021-11-07 09:39:59 -0300

    feat: criando métodos para add remover e limpar carrinho

--- src/context/Cart/index.tsx
+++ src/context/Cart/index.tsx
@@ -1,7 +1,7 @@
 import { useState, useEffect, createContext } from 'react';
 
 import { useQueryGames } from 'graphql/queries/games';
-import { getStorageItem } from 'utils/localStorage';
+import { getStorageItem, setStorageItem } from 'utils/localStorage';
 import { cartMapper } from 'utils/mappers';
 import formatPrice from 'utils/format-price';
 
@@ -16,6 +16,11 @@ export type CartContextProps = {
   items: CartItem[];
   quantity: number;
   total: string;
+  isInCart: (id: string) => boolean;
+  addToCart: (id: string) => void;
+  removeFromCart: (id: string) => void;
+  clearCart: () => void;
+  loading: boolean;
 };
 
 const CartContext = createContext({} as CartContextProps);
@@ -27,7 +32,7 @@ export type CartProviderProps = {
 const CART_KEY = 'cartItems';
 
 const CartProvider = ({ children }: CartProviderProps) => {
-  const [cartItems, setCartItems] = useState<CartItem[]>([]);
+  const [cartItems, setCartItems] = useState<string[]>([]);
 
   useEffect(() => {
     const data = getStorageItem(CART_KEY);
@@ -37,7 +42,7 @@ const CartProvider = ({ children }: CartProviderProps) => {
     }
   }, []);
 
-  const { data } = useQueryGames({
+  const { data, loading } = useQueryGames({
     skip: !cartItems?.length,
     variables: {
       where: {
@@ -50,12 +55,37 @@ const CartProvider = ({ children }: CartProviderProps) => {
     return acc + game.price;
   }, 0);
 
+  const isInCart = (id: string) => (id ? cartItems.includes(id) : false);
+
+  const saveCart = (cartItems: string[]) => {
+    setCartItems(cartItems);
+    setStorageItem(CART_KEY, cartItems);
+  };
+
+  const addToCart = (id: string) => {
+    saveCart([...cartItems, id]);
+  };
+
+  const removeFromCart = (id: string) => {
+    const newCartItems = cartItems.filter((itemId: string) => itemId !== id);
+    saveCart(newCartItems);
+  };
+
+  const clearCart = () => {
+    saveCart([]);
+  };
+
   return (
     <CartContext.Provider
       value={{
         items: cartMapper(data?.games),
         quantity: cartItems.length,
         total: formatPrice(total || 0),
+        isInCart,
+        addToCart,
+        removeFromCart,
+        clearCart,
+        loading,
       }}
     >
       {children}
--- src/hooks/useCart/test.tsx
+++ src/hooks/useCart/test.tsx
@@ -1,4 +1,5 @@
 import { renderHook } from '@testing-library/react-hooks';
+import { act } from '@testing-library/react';
 
 import { MockedProvider } from '@apollo/client/testing';
 import { setStorageItem } from 'utils/localStorage';
@@ -10,6 +11,10 @@ import useCart from '.';
 import { cartItems, gamesMock } from './mock';
 
 describe('useCart', () => {
+  beforeEach(() => {
+    window.localStorage.clear();
+  });
+
   it('should display an error when not passing the provider', () => {
     const wrapper = ({ children }: CartProviderProps) => (
       <MockedProvider mocks={[gamesMock]}>{children}</MockedProvider>
@@ -37,10 +42,98 @@ describe('useCart', () => {
       wrapper,
     });
 
+    expect(result.current.loading).toBe(true);
+
     await waitForNextUpdate();
+
+    expect(result.current.loading).toBe(false);
 
     expect(result.current.items).toStrictEqual(cartItems);
     expect(result.current.quantity).toBe(2);
     expect(result.current.total).toBe('$21.00');
+  });
+
+  it('should return true/false if the item is already in the cart', () => {
+    const wrapper = ({ children }: CartProviderProps) => (
+      <MockedProvider mocks={[gamesMock]}>
+        <CartProvider>{children}</CartProvider>
+      </MockedProvider>
+    );
+
+    setStorageItem('cartItems', ['1']);
+
+    const { result } = renderHook(() => useCart(), {
+      wrapper,
+    });
+
+    expect(result.current.isInCart('1')).toBe(true);
+    expect(result.current.isInCart('2')).toBe(false);
+  });
+
+  it('should add item in the cart', () => {
+    const wrapper = ({ children }: CartProviderProps) => (
+      <MockedProvider mocks={[gamesMock]}>
+        <CartProvider>{children}</CartProvider>
+      </MockedProvider>
+    );
+
+    const { result } = renderHook(() => useCart(), {
+      wrapper,
+    });
+
+    act(() => {
+      result.current.addToCart('1');
+    });
+
+    expect(result.current.quantity).toBe(1);
+    expect(window.localStorage.getItem('WONGAMES_cartItems')).toBe(
+      JSON.stringify(['1']),
+    );
+  });
+
+  it('should remove and item from the cart', () => {
+    const wrapper = ({ children }: CartProviderProps) => (
+      <MockedProvider mocks={[gamesMock]}>
+        <CartProvider>{children}</CartProvider>
+      </MockedProvider>
+    );
+
+    setStorageItem('cartItems', ['1']);
+
+    const { result } = renderHook(() => useCart(), {
+      wrapper,
+    });
+
+    act(() => {
+      result.current.removeFromCart('1');
+    });
+
+    expect(result.current.quantity).toBe(0);
+    expect(window.localStorage.getItem('WONGAMES_cartItems')).toBe(
+      JSON.stringify([]),
+    );
+  });
+
+  it('should clear the cart', () => {
+    const wrapper = ({ children }: CartProviderProps) => (
+      <MockedProvider mocks={[gamesMock]}>
+        <CartProvider>{children}</CartProvider>
+      </MockedProvider>
+    );
+
+    setStorageItem('cartItems', ['1']);
+
+    const { result } = renderHook(() => useCart(), {
+      wrapper,
+    });
+
+    act(() => {
+      result.current.clearCart();
+    });
+
+    expect(result.current.quantity).toBe(0);
+    expect(window.localStorage.getItem('WONGAMES_cartItems')).toBe(
+      JSON.stringify([]),
+    );
   });
 });
