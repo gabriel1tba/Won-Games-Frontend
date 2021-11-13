853f61f60410665beb694049820a21349d3d0c73
Gabriel Ferreira <gabriel.ferreira.itba@gmail.com>
2021-11-13 12:25:15 -0300

    feat: adicionando AuthProvider com NextAuth

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,6 +2,7 @@ import NextNprogress from 'nextjs-progressbar';
 import Head from 'next/head';
 import { AppProps } from 'next/app';
 import AppProvider from 'context';
+import { Provider as AuthProvider } from 'next-auth/client';
 import { ApolloProvider } from '@apollo/client';
 import { ThemeProvider } from 'styled-components';
 
@@ -13,31 +14,33 @@ function App({ Component, pageProps }: AppProps) {
   const client = useApollo(pageProps.initialApoloState);
 
   return (
-    <ApolloProvider client={client}>
-      <ThemeProvider theme={theme}>
-        <AppProvider>
-          <Head>
-            <title>Won Games</title>
-            <link rel="shortcut icon" href="/img/icon-512.png" />
-            <link rel="apple-touch-icon" href="/img/icon-512.png" />
-            <link rel="manifest" href="/manifest.json" />
-            <meta name="theme-color" content="#06092B" />
-            <meta
-              name="description"
-              content="The best Game Stores in the world!"
+    <AuthProvider session={pageProps.session}>
+      <ApolloProvider client={client}>
+        <ThemeProvider theme={theme}>
+          <AppProvider>
+            <Head>
+              <title>Won Games</title>
+              <link rel="shortcut icon" href="/img/icon-512.png" />
+              <link rel="apple-touch-icon" href="/img/icon-512.png" />
+              <link rel="manifest" href="/manifest.json" />
+              <meta name="theme-color" content="#06092B" />
+              <meta
+                name="description"
+                content="The best Game Stores in the world!"
+              />
+            </Head>
+            <GlobalStyles />
+            <NextNprogress
+              color="#F231A5"
+              startPosition={0.3}
+              stopDelayMs={200}
+              height={3}
             />
-          </Head>
-          <GlobalStyles />
-          <NextNprogress
-            color="#F231A5"
-            startPosition={0.3}
-            stopDelayMs={200}
-            height={3}
-          />
-          <Component {...pageProps} />
-        </AppProvider>
-      </ThemeProvider>
-    </ApolloProvider>
+            <Component {...pageProps} />
+          </AppProvider>
+        </ThemeProvider>
+      </ApolloProvider>
+    </AuthProvider>
   );
 }
 
