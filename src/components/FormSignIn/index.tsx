a48f20ba19f84c3c8ee7db1bda845c39171adc41
Gabriel Ferreira <gabriel.ferreira.itba@gmail.com>
2021-11-13 15:05:23 -0300

    feat: adicionando loading ao form SignIn

--- src/components/FormSignIn/index.tsx
+++ src/components/FormSignIn/index.tsx
@@ -7,13 +7,14 @@ import { Email, Lock } from '@styled-icons/material-outlined';
 import Button from 'components/Button';
 import TextField from 'components/Textfield';
 
-import { FormWrapper, FormLink } from '../Form';
+import { FormWrapper, FormLink, FormLoading } from '../Form';
 
 import * as S from './styles';
 
 const FormSignIn = () => {
-  const [values, setValues] = useState({});
   const { push } = useRouter();
+  const [values, setValues] = useState({});
+  const [loading, setLoading] = useState(false);
 
   const handleInput = (field: string, value: string) => {
     setValues((s) => ({ ...s, [field]: value }));
@@ -21,6 +22,7 @@ const FormSignIn = () => {
 
   const handleSubmit = async (event: React.FormEvent) => {
     event.preventDefault();
+    setLoading(true);
 
     // sign in
     const result = await signIn('credentials', {
@@ -32,6 +34,8 @@ const FormSignIn = () => {
     if (result?.url) {
       return push(result?.url);
     }
+
+    setLoading(false);
 
     // jogar o erro
     console.error('email ou senha inválida');
@@ -56,8 +60,8 @@ const FormSignIn = () => {
         />
         <S.ForgotPassword href="#">Forgot your password?</S.ForgotPassword>
 
-        <Button type="submit" size="large" fullWidth>
-          Sign in now
+        <Button type="submit" size="large" fullWidth disabled={loading}>
+          {loading ? <FormLoading /> : <span>Sign in now</span>}
         </Button>
 
         <FormLink>
