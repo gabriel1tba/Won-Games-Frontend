ab8b60a435146a01d872c9fda4f9d7237feac4b1
Gabriel Ferreira <gabriel.ferreira.itba@gmail.com>
2021-11-13 09:03:36 -0300

    fix: removendo estilo de autofill nos inputs

--- src/components/Textfield/styles.ts
+++ src/components/Textfield/styles.ts
@@ -9,26 +9,14 @@ type WrapperProps = Pick<TextFieldProps, 'disabled'> & { error?: boolean };
 export const InputWrapper = styled.div`
   ${({ theme }) => css`
     display: flex;
+    align-items: center;
     background: ${theme.colors.lightGray};
     border-radius: 0.2rem;
     padding: 0 ${theme.spacings.xsmall};
     border: 0.2rem solid;
     border-color: ${theme.colors.lightGray};
-
     &:focus-within {
       box-shadow: 0 0 0.5rem ${theme.colors.primary};
-    }
-  `}
-`;
-
-export const Icon = styled.div<IconPositionProps>`
-  ${({ theme, iconPosition }) => css`
-    display: flex;
-    width: 2.2rem;
-    color: ${theme.colors.gray};
-    order: ${iconPosition === 'right' ? 1 : 0};
-    & > svg {
-      width: 100%;
     }
   `}
 `;
@@ -43,7 +31,12 @@ export const Input = styled.input<IconPositionProps>`
     background: transparent;
     border: 0;
     outline: none;
-    width: 100%;
+    width: ${iconPosition === 'right' ? `calc(100% - 2.2rem)` : `100%`};
+    &:-webkit-autofill {
+      -webkit-box-shadow: 0 0 0 ${theme.spacings.small}
+        ${theme.colors.lightGray} inset;
+      filter: none
+    }
   `}
 `;
 
@@ -52,6 +45,18 @@ export const Label = styled.label`
     font-size: ${theme.font.sizes.small};
     color: ${theme.colors.black};
     cursor: pointer;
+  `}
+`;
+
+export const Icon = styled.div<IconPositionProps>`
+  ${({ theme, iconPosition }) => css`
+    display: flex;
+    color: ${theme.colors.gray};
+    order: ${iconPosition === 'right' ? 1 : 0};
+    & > svg {
+      width: 2.2rem;
+      height: 100%;
+    }
   `}
 `;
 
