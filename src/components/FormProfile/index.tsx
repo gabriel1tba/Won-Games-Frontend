47f141d3acc90701657f674fe74e1e92c53cfc31
Gabriel Ferreira <gabriel.ferreira.itba@gmail.com>
2021-11-14 15:35:12 -0300

    feat: recebendo dados do usuário logado

--- src/components/FormProfile/index.tsx
+++ src/components/FormProfile/index.tsx
@@ -3,8 +3,12 @@ import Heading from 'components/Heading';
 import TextField from 'components/Textfield';
 
 import * as S from './styles';
+export type FormProfileProps = {
+  username?: string;
+  email?: string;
+};
 
-const FormProfile = () => (
+const FormProfile = ({ email, username }: FormProfileProps) => (
   <>
     <Heading lineBottom color="black" size="small">
       My profile
@@ -12,17 +16,17 @@ const FormProfile = () => (
 
     <S.Form>
       <TextField
-        name="name"
-        placeholder="Name"
-        label="Name"
-        initialValue="John Doe"
+        name="username"
+        placeholder="Username"
+        label="Username"
+        initialValue={username}
       />
 
       <TextField
         name="email"
         type="email"
         placeholder="E-mail"
-        initialValue="johndoe@gmail.com"
+        initialValue={email}
         label="E-mail"
         disabled
       />
--- src/pages/profile/me.tsx
+++ src/pages/profile/me.tsx
@@ -1,12 +1,16 @@
 import { GetServerSidePropsContext } from 'next';
-import FormProfile from 'components/FormProfile';
+
+import FormProfile, { FormProfileProps } from 'components/FormProfile';
 import Profile from 'templates/Profile';
 import protectedRoutes from 'utils/protected-routes';
+import { initializeApollo } from 'utils/apollo';
+import { QueryProfileMe } from 'graphql/generated/QueryProfileMe';
+import { QUERY_PROFILE_ME } from 'graphql/queries/profile';
 
-const Me = () => {
+const Me = (props: FormProfileProps) => {
   return (
     <Profile>
-      <FormProfile />
+      <FormProfile {...props} />
     </Profile>
   );
 };
@@ -17,10 +21,17 @@ export const getServerSideProps = async (
   context: GetServerSidePropsContext,
 ) => {
   const session = await protectedRoutes(context);
+  const apolloClient = initializeApollo(null, session);
+
+  const { data } = await apolloClient.query<QueryProfileMe>({
+    query: QUERY_PROFILE_ME,
+  });
 
   return {
     props: {
       session,
+      username: data.me?.username,
+      email: data.me?.email,
     },
   };
 };
