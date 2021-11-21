a3d6a9a7a8e01d239ecf0aa65a7cca1788238cd5
Gabriel Ferreira <gabriel.ferreira.itba@gmail.com>
2021-11-20 21:23:50 -0300

    feat: componente WishlistButton com tests

---
+++ src/components/WishlistButton/test.tsx
@@ -0,0 +1,104 @@
+import { act, render, screen } from 'utils/test-utils';
+import userEvent from '@testing-library/user-event';
+
+import WishlistButton from '.';
+
+import { WishlistContextMock } from 'context/Wishlist';
+
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const useSession = jest.spyOn(require('next-auth/client'), 'useSession');
+const session = { jwt: '123', user: { email: 'lorem@ipsum.com' } };
+useSession.mockImplementation(() => [session]);
+
+describe('<WishlistButton />', () => {
+  it('should render a button to add to wishlist', () => {
+    const wishlistProviderProps = {
+      ...WishlistContextMock,
+      isInWishlist: () => false,
+    };
+
+    render(<WishlistButton id="1" />, { wishlistProviderProps });
+
+    expect(screen.getByLabelText(/add to wishlist/i)).toBeInTheDocument();
+  });
+
+  it('should render a button to remove from wishlist', () => {
+    const wishlistProviderProps = {
+      ...WishlistContextMock,
+      isInWishlist: () => true,
+    };
+
+    render(<WishlistButton id="1" />, { wishlistProviderProps });
+
+    expect(screen.getByLabelText(/remove from wishlist/i)).toBeInTheDocument();
+  });
+
+  it('should render a button with add to wishlist text', () => {
+    const wishlistProviderProps = {
+      ...WishlistContextMock,
+      isInWishlist: () => false,
+    };
+
+    render(<WishlistButton id="1" hasText />, { wishlistProviderProps });
+
+    expect(screen.getByText(/add to wishlist/i)).toBeInTheDocument();
+  });
+
+  it('should render a button with remove from wishlist text', () => {
+    const wishlistProviderProps = {
+      ...WishlistContextMock,
+      isInWishlist: () => true,
+    };
+
+    render(<WishlistButton id="1" hasText />, { wishlistProviderProps });
+
+    expect(screen.getByText(/remove from wishlist/i)).toBeInTheDocument();
+  });
+
+  it('should not render if not logged', () => {
+    // eslint-disable-next-line @typescript-eslint/no-var-requires
+    const useSession = jest.spyOn(require('next-auth/client'), 'useSession');
+    useSession.mockImplementationOnce(() => [null]);
+
+    const wishlistProviderProps = {
+      ...WishlistContextMock,
+      isInWishlist: () => true,
+    };
+
+    render(<WishlistButton id="1" hasText />, { wishlistProviderProps });
+
+    expect(screen.queryByText(/remove from wishlist/i)).not.toBeInTheDocument();
+  });
+
+  it('should add to wishlist', async () => {
+    const wishlistProviderProps = {
+      ...WishlistContextMock,
+      isInWishlist: () => false,
+      addToWishlist: jest.fn(),
+    };
+
+    render(<WishlistButton id="1" hasText />, { wishlistProviderProps });
+
+    act(() => {
+      userEvent.click(screen.getByText(/add to wishlist/i));
+    });
+
+    expect(wishlistProviderProps.addToWishlist).toHaveBeenCalledWith('1');
+  });
+
+  it('should remove from wishlist', async () => {
+    const wishlistProviderProps = {
+      ...WishlistContextMock,
+      isInWishlist: () => true,
+      removeFromWishlist: jest.fn(),
+    };
+
+    render(<WishlistButton id="1" hasText />, { wishlistProviderProps });
+
+    act(() => {
+      userEvent.click(screen.getByText(/remove from wishlist/i));
+    });
+
+    expect(wishlistProviderProps.removeFromWishlist).toHaveBeenCalledWith('1');
+  });
+});
