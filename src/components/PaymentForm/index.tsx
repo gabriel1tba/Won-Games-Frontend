c736cedcffdc538a80c414bb96b3025abce6f2a3
Gabriel Ferreira <gabriel.ferreira.itba@gmail.com>
2021-11-22 22:51:16 -0300

    feat: adicionando validação

--- src/components/PaymentForm/index.tsx
+++ src/components/PaymentForm/index.tsx
@@ -1,5 +1,7 @@
+import { useState } from 'react';
 import { CardElement } from '@stripe/react-stripe-js';
-import { ShoppingCart } from '@styled-icons/material-outlined';
+import { StripeCardElementChangeEvent } from '@stripe/stripe-js';
+import { ErrorOutline, ShoppingCart } from '@styled-icons/material-outlined';
 
 import Button from 'components/Button';
 import Heading from 'components/Heading';
@@ -7,13 +9,29 @@ import Heading from 'components/Heading';
 import * as S from './styles';
 
 const PaymentForm = () => {
+  const [error, setError] = useState<string | null>(null);
+
+  const handleChange = async (event: StripeCardElementChangeEvent) => {
+    setError(event.error ? event.error.message : '');
+  };
+
   return (
     <S.Wrapper>
       <S.Body>
         <Heading color="black" size="small" lineBottom>
           Payment
         </Heading>
-        <CardElement options={{ hidePostalCode: true }} />
+        <CardElement
+          options={{ hidePostalCode: true }}
+          onChange={handleChange}
+        />
+
+        {error && (
+          <S.Error>
+            <ErrorOutline size={20} />
+            {error}
+          </S.Error>
+        )}
       </S.Body>
       <S.Footer>
         <Button as="a" fullWidth minimal>
