2b116d037fc8e4e05340f583884f2d46d4b58488
Gabriel Ferreira <gabriel.ferreira.itba@gmail.com>
2021-11-25 07:26:02 -0300

    feat: melhorias de acessibilidade

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -16,12 +16,12 @@ const Footer = () => (
         <a href="mailto:sac@wongames.com">sac@wongames.com</a>
       </S.Column>
 
-      <S.Column>
+      <S.Column aria-labelledby="social-media">
         <Heading color="black" lineColor="secondary" lineBottom size="small">
           Follow us
         </Heading>
 
-        <nav aria-labelledby="social media">
+        <nav id="social-media">
           <a
             href="https://www.instagram.com/won-games"
             target="_blank"
@@ -53,12 +53,12 @@ const Footer = () => (
         </nav>
       </S.Column>
 
-      <S.Column>
+      <S.Column aria-labelledby="resources">
         <Heading color="black" lineColor="secondary" lineBottom size="small">
           Links
         </Heading>
 
-        <nav aria-labelledby="footer resources">
+        <nav id="resources">
           <Link href="/">
             <a>Home</a>
           </Link>
@@ -71,7 +71,7 @@ const Footer = () => (
         </nav>
       </S.Column>
 
-      <S.Column aria-labelledby="footer-contact">
+      <S.Column aria-label="contact">
         <Heading color="black" lineColor="secondary" lineBottom size="small">
           Location
         </Heading>
--- src/components/Footer/styles.ts
+++ src/components/Footer/styles.ts
@@ -1,5 +1,6 @@
 import styled, { css } from 'styled-components';
 import media from 'styled-media-query';
+import { darken } from 'polished';
 
 import * as HeadingStyles from 'components/Heading/styles';
 
@@ -26,10 +27,10 @@ export const Column = styled.div`
     a,
     span {
       display: block;
-      color: ${theme.colors.gray};
+      color: ${darken(0.2, theme.colors.gray)};
       text-decoration: none;
       margin-bottom: ${theme.spacings.xxsmall};
-      font-size: ${theme.font.sizes.small};
+      font-size: ${theme.font.sizes.medium};
     }
     a:hover {
       text-decoration: underline;
