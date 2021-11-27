38e97ab62173e7143a8c2da89a9cc0500d84f2fc
Gabriel Ferreira <gabriel.ferreira.itba@gmail.com>
2021-11-27 12:45:42 -0300

    feat: criando testes para página Game no cypress

--- cypress/e2e/home.ts
+++ cypress/e2e/home.ts
@@ -5,9 +5,9 @@ describe('Home Page', () => {
     cy.visit('/');
 
     cy.shouldRenderBanner();
-    cy.shouldRenderShowcase({ name: "New Games", hightlight: false })
-    cy.shouldRenderShowcase({ name: "Most Popular Games", hightlight: true })
-    cy.shouldRenderShowcase({ name: "Upcoming Games", hightlight: true })
-    cy.shouldRenderShowcase({ name: "Free Games", hightlight: true })
+    cy.shouldRenderShowcase({ name: 'New Games', hightlight: false });
+    cy.shouldRenderShowcase({ name: 'Most Popular Games', hightlight: true });
+    cy.shouldRenderShowcase({ name: 'Upcoming Games', hightlight: true });
+    cy.shouldRenderShowcase({ name: 'Free Games', hightlight: true });
   });
 });
