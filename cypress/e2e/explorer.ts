f11497608d178a9db6782627a0a99bfdd605ecbb
Gabriel Ferreira <gabriel.ferreira.itba@gmail.com>
2021-11-27 15:06:18 -0300

    feat: testando filtro de preço na página Explorer no cypress

--- cypress/e2e/explorer.ts
+++ cypress/e2e/explorer.ts
@@ -8,6 +8,10 @@ import {
 } from '../../src/utils/filter/fields';
 
 describe('Explore Page', () => {
+  before(() => {
+    cy.visit('/games');
+  });
+
   it('should render filters columns', () => {
     cy.visit('/games');
 
@@ -20,5 +24,31 @@ describe('Explore Page', () => {
     cy.getFields(priceFields);
     cy.getFields(platformFields);
     cy.getFields(genreFields);
+  });
+
+  it('should show 15 games and show more games when show more is clicked', () => {
+    cy.getByDataCy('game-card').should('have.length', 9);
+    cy.findByRole('button', { name: /show more/i }).click();
+    cy.getByDataCy('game-card').should('have.length', 18);
+  });
+
+  it('should order by price', () => {
+    cy.findByText(/lowest to highest/i).click();
+    cy.location('href').should('contain', 'sort=price%3Aasc');
+
+    cy.getByDataCy('game-card')
+      .first()
+      .within(() => {
+        cy.findByText('$0.00').should('exist');
+      });
+
+    cy.findByText(/highest to lowest/i).click();
+    cy.location('href').should('contain', 'sort=price%3Adesc');
+
+    cy.getByDataCy('game-card')
+      .first()
+      .within(() => {
+        cy.findByText('$0.00').should('not.exist');
+      });
   });
 });
